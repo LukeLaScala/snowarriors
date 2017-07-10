b959101adbba019ddc5366017909603f5e15dc8c
Luke LaScala <lascala.luke@gmail.com>
2017-07-09 21:16:26 -0400

    jowls

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
       <div class="container">
         <div class="row">
           <div class="col l6 s12">
-            <h5 class="white-text">Indian Lake Snowarriors</h5>
+            <h5><a class="white-text" href="index.php">Indian Lake Snowarriors</a></h5>
             <p class="grey-text text-lighten-4"></p>
           </div>
           <div class="col l4 offset-l2 s12">
