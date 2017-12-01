9c50d2a6590dbd9df57ca14ff4a112dc7ac7799e
Luke LaScala <lascala.luke@gmail.com>
2017-12-01 09:38:21 -0500

    wow

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
               </div>
               <div class="col l4">
                 <p class="black-text right">
-                    <?php echo(date('M j Y', strtotime($news['timestamp']))); ?>
+                   Posted on <?php echo(date('M j Y', strtotime($news['timestamp']))); ?>
                 </p>
               </div>
             </div>
