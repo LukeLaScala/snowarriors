77053c241146f5af4ba0b71400e2cdb5d4b65c13
Luke LaScala <lascala.luke@gmail.com>
2017-12-02 18:49:40 -0500

    wow

--- includes/head.php
+++ includes/head.php
@@ -3,4 +3,5 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>    
+    <link href="https://fonts.googleapis.com/css?family=Lato:700i" rel="stylesheet">
 </head>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,16 +1,17 @@
   <nav id="nav" style="z-index: 999; opacity: .9; position: relative;">
       <div class="nav-wrapper white">
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-        <a href="index.php" style="padding-left: 2rem;"><img style="padding-top: 5px; width: 60px; height: 65px;" src="static/img/snowarriors.png"></a>
+        <a href="/"><img onclick="window.location='http://snowarriors.com'" style="padding-left: 40px; padding-top: 5px; width: 125px; height: 90px;" src="static/img/snowarriors.png"></a>
+        <a style="padding-left: 20px; font-family: 'Lato', sans-serif; color: #800020" class="brand-logo" href="https://snowarriors.com">Indian Lake Snowarriors</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#!" data-activates="aboutusdrop" class="dropdown-button green-text larger">About Us <i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="#!" data-activates="aboutusdrop" class="dropdown-button black-text larger">About Us <i class="material-icons right">arrow_drop_down</i></a>
           </li>
-          <li><a href="#!" data-activates="membershipdrop" class="dropdown-button green-text larger">Membership<i class="material-icons right">arrow_drop_down</i></a>
-          <li><a href="#!" data-activates="mediadrop" class="green-text dropdown-button larger">Media<i class="material-icons right">arrow_drop_down</i></a>
-          <li><a href="events.php" class="larger green-text">Events</a></li>
+          <li><a href="#!" data-activates="membershipdrop" class="dropdown-button black-text larger">Membership<i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="#!" data-activates="mediadrop" class="black-text dropdown-button larger">Media<i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="events.php" class="larger black-text">Events</a></li>
           <!-- <li><a href="shop.php" class="larger">Shop</a></li> -->
-          <li><a href="links.php" class="larger green-text">Important Links</a></li>
-          <li><a href="contact.php" class="larger green-text">Contact Us</a></li>
+          <li><a href="links.php" class="larger black-text">Important Links</a></li>
+          <li><a href="contact.php" class="larger black-text">Contact Us</a></li>
     		</ul>
         <ul class="side-nav" id="mobile-demo">
           <li><a href="about.php">About Us</a></li>
@@ -24,18 +25,18 @@
 
 <!-- About Us Drop -->
 <ul id="aboutusdrop" class="dropdown-content white">
-  <li><a class="green-text" href="officersanddirectors.php">Officers & Directors</a></li>
-  <li><a class="green-text" href="dougwells.php">Doug Wells Remembered</a></li>
+  <li><a class="black-text" href="officersanddirectors.php">Officers & Directors</a></li>
+  <li><a class="black-text" href="dougwells.php">Doug Wells Remembered</a></li>
 </ul>
 <!-- Membership Drop -->
 <ul id="membershipdrop" class="dropdown-content white">
-  <li><a class="green-text" href="membershipform.php">Membership Form</a></li>
-  <li><a class="green-text" target="_" href="https://membership.nysnowmobiler.com/clubs/510">Online Registration</a></li>
+  <li><a class="black-text" href="membershipform.php">Membership Form</a></li>
+  <li><a class="black-text" target="_" href="https://membership.nysnowmobiler.com/clubs/510">Online Registration</a></li>
 </ul>
 
 <!-- Media Drop -->
 <ul id="mediadrop" class="dropdown-content white">
-  <li><a class="green-text" href="photosandvideos.php">Photos & Videos</a></li>
-    <li><a class="green-text" href="journal.php">Snowarrior Journal</a></li>
+  <li><a class="black-text" href="photosandvideos.php">Photos & Videos</a></li>
+    <li><a class="black-text" href="journal.php">Snowarrior Journal</a></li>
 
 </ul>
