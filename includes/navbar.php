6f6ed822576db982584a54242e4c2a6cacdb87ff
Luke LaScala <lascala.luke@gmail.com>
2017-12-02 19:07:15 -0500

    wow

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,16 +2,16 @@
       <div class="nav-wrapper white">
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <a href="/"><img onclick="window.location='http://snowarriors.com'" style="padding-left: 40px; padding-top: 5px; width: 125px; height: 90px;" src="static/img/snowarriors.png"></a>
-        <a style="padding-left: 20px; font-family: 'Lato', sans-serif; font-size: 35px; color: #800020" class="brand-logo" href="https://snowarriors.com">Indian Lake Snowarriors</a>
+        <a style="padding-left: 20px; font-family: 'Lato', sans-serif; font-size: 35px; color: #800020" class="brand-logo" href="https://snowarriors.com">INDIAN LAKE SNOWARRIORS</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#!" data-activates="aboutusdrop" class="dropdown-button black-text larger">About Us <i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="#!" data-activates="aboutusdrop" class="dropdown-button black-text larger">ABOUT US <i class="material-icons right">arrow_drop_down</i></a>
           </li>
-          <li><a href="#!" data-activates="membershipdrop" class="dropdown-button black-text larger">Membership<i class="material-icons right">arrow_drop_down</i></a>
-          <li><a href="#!" data-activates="mediadrop" class="black-text dropdown-button larger">Media<i class="material-icons right">arrow_drop_down</i></a>
-          <li><a href="events.php" class="larger black-text">Events</a></li>
+          <li><a href="#!" data-activates="membershipdrop" class="dropdown-button black-text larger">MEMBERSHIP<i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="#!" data-activates="mediadrop" class="black-text dropdown-button larger">MEDIA<i class="material-icons right">arrow_drop_down</i></a>
+          <li><a href="events.php" class="larger black-text">EVENTS</a></li>
           <!-- <li><a href="shop.php" class="larger">Shop</a></li> -->
-          <li><a href="links.php" class="larger black-text">Important Links</a></li>
-          <li><a href="contact.php" class="larger black-text">Contact Us</a></li>
+          <li><a href="links.php" class="larger black-text">IMPORTANT LINKS</a></li>
+          <li><a href="contact.php" class="larger black-text">CONTACT US</a></li>
     		</ul>
         <ul class="side-nav" id="mobile-demo">
           <li><a href="about.php">About Us</a></li>
@@ -25,18 +25,18 @@
 
 <!-- About Us Drop -->
 <ul id="aboutusdrop" class="dropdown-content white">
-  <li><a class="black-text" href="officersanddirectors.php">Officers & Directors</a></li>
-  <li><a class="black-text" href="dougwells.php">Doug Wells Remembered</a></li>
+  <li><a class="black-text" href="officersanddirectors.php">OFFICERS & DIRECTORS</a></li>
+  <li><a class="black-text" href="dougwells.php">DOUG WELLS REMEMBERS</a></li>
 </ul>
 <!-- Membership Drop -->
 <ul id="membershipdrop" class="dropdown-content white">
-  <li><a class="black-text" href="membershipform.php">Membership Form</a></li>
-  <li><a class="black-text" target="_" href="https://membership.nysnowmobiler.com/clubs/510">Online Registration</a></li>
+  <li><a class="black-text" href="membershipform.php">MEMBERSHIP FORM</a></li>
+  <li><a class="black-text" target="_" href="https://membership.nysnowmobiler.com/clubs/510">ONLINE REGISTRATION</a></li>
 </ul>
 
 <!-- Media Drop -->
 <ul id="mediadrop" class="dropdown-content white">
-  <li><a class="black-text" href="photosandvideos.php">Photos & Videos</a></li>
-    <li><a class="black-text" href="journal.php">Snowarrior Journal</a></li>
+  <li><a class="black-text" href="photosandvideos.php">PHOTOS & VIDEOS</a></li>
+    <li><a class="black-text" href="journal.php">SNOWARRIOR JOURNAL</a></li>
 
 </ul>
